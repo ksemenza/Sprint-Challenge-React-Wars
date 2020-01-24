2ea0b8c04f5b5e30482163269a67cde58de2ad31
ksemenza27 <ksemenza27@outlook.com>
2020-01-24 12:47:01 -0500

    mvp completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import StarWarCard from "./components/StarWarsCard"
+import styled from "styled-components"
 import './App.css';
+import axios from "axios"
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [starWars, setStarWars] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  
+ const Cta = styled.div `
+ display: grid;
+ grid-gap: 20px;
+ grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+ margin: 20px auto;
+ max-width: 900px;
+
+`
+useEffect(() => {
+  axios
+  .get("https://swapi.co/api/people/")
+  .then(res => {
+    console.log(res.data.results)
+    setStarWars(res.data.results)
+   
+  })
+  .catch(error => {
+    console.log ("starwar res.data", error)
+  });
+},[]);
+ useEffect(() => {}, []);
+
+
+
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+<div>
+    <h1 className="Header">React Wars</h1>
+    <Cta>
+   
+      {starWars.map(sw => {
+        return(
+    
+          <StarWarCard
+          name={sw.name}
+          height={sw.height}
+          mass={sw.mass}
+          />
+       
+        )
+      })}
+
+</Cta>
     </div>
+
   );
 }
 
